e3d9d62eb78a57681e0a517c1bae51c9e49dc271
Dominic852 <43085764+Dominic852@users.noreply.github.com>
2020-12-08 20:31:52 +0800

    [React-ToDoList] refactor: reformatting

--- src/reducers.js
+++ src/reducers.js
@@ -3,13 +3,13 @@ import { ADD_TODO_ITEM, UPDATE_ITEM_STATUS, DELETE_ITEM } from './actionTypes'
 
 const ToDos = (state = [], action) => {
     if (action.type === ADD_TODO_ITEM) {
-        return state.concat(action.payload);
+        return [action.payload,...state];
     }
     if (action.type === UPDATE_ITEM_STATUS) {
         return state.map(todo => todo.id === action.id ? { ...todo, done: !action.status } : todo);
     }
     if (action.type === DELETE_ITEM) {
-        return state.filter(todo => todo.id != action.id);
+        return state.filter(todo => todo.id !== action.id);
     }
     return state;
 
