b651789b6a59ad09834970e780ff4cd30926cfcd
Dominic852 <43085764+Dominic852@users.noreply.github.com>
2020-12-09 10:43:56 +0800

    [React-ToDoList] feat: add enter function to add todo item

--- src/components/ToDoGenerator.js
+++ src/components/ToDoGenerator.js
@@ -14,13 +14,18 @@ class ToDoGenerator extends Component {
             text: ""
         })
     }
+    handleKeyDown = (event)=>{
+        if (event.key==="Enter"){
+            this.addToDoItem();
+        }
+    }
     changeText = (event) => {
         this.setState({ text: event.target.value });
     }
     render() {
         return (
             <div>
-                <input type="text" value={this.state.text} placeholder="Input some text here" onChange={this.changeText} />
+                <input type="text" value={this.state.text} placeholder="Input some text here" onChange={this.changeText} onKeyDown={this.handleKeyDown}/>
                 <input type="button" value="add" onClick={this.addToDoItem}/>
             </div>
         );
