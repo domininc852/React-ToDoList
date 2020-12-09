57046f45e7a9fc0bff4de517fadcc5b3dc4b4084
Dominic852 <43085764+Dominic852@users.noreply.github.com>
2020-12-09 12:40:20 +0800

    [React-ToDoList] feat: modify css

--- src/components/DoneList.js
+++ src/components/DoneList.js
@@ -3,11 +3,14 @@ import React, { Component } from 'react';
 class DoneList extends Component {
     render() {
         return (
-            <ul>
-                {this.props.doneList.map(doneItem => {
-                    return <li>{doneItem.text}</li>;
-                })}
-            </ul>
+            <div className="DoneList">
+                <h1>Done List</h1>
+                <ul style={{display: "table", margin:"0 auto"}}>
+                    {this.props.doneList.map(doneItem => {
+                        return <li key={doneItem.id}>{doneItem.text}</li>;
+                    })}
+                </ul>
+            </div>
         );
     }
 }
